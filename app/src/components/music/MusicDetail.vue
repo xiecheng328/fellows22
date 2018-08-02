12015d970e3e74c2fc35c1a41e1a576b727d9ad8
dongxiuling <dxl_web@163.com>
2018-08-02 14:48:52 +0800

    app

--- app/src/components/music/MusicDetail.vue
+++ app/src/components/music/MusicDetail.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
-        {{$route.params.musicId}}
+        <aplayer autoplay
+  :music="{
+    title: 'secret base~君がくれたもの~',
+    artist: 'Silent Siren',
+    src: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3',
+    pic: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
+  }"
+/>
+        <!-- {{$route.params.musicId}} -->
     </div>
 </template>
 
 <script>
+    import aplayer from 'vue-aplayer'
+    import axios from 'axios'
     export default {
+        components: {
+            aplayer
+        },
+        created() {
+            axios.get('./static/data/musicdata.json').then(res =>{
+                console.log(res.data.musicData);
+            }).catch(res => {
+                
+            });
+
+            [{
+                src:'',
+                title:''
+                artist:''
+            }]
+        }
+
+
+
+
         
     }
 </script>
